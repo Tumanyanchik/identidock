9c96225d181fedf286f1dfd682cd9f531daea33a
Tumanyanchik <tumanyan2000@yandex.ru>
2026-05-04 12:05:24 +0300

    Initial commit: simple web application

---
+++ identidock/cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+if [ "$ENV" = 'DEV' ]; then
+   echo "Running Development Server" # Запуск сервера для разработки
+   exec python "identidock.py"
+else
+   echo "Running Production Server" # Запуск сервера для эксплуатации
+   exec uwsgi --ini /app/uwsgi.ini
+fi
